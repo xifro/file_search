606f0a35ad051c0e54834e276cd0822cf33b5363
Oleksandr Samoilenko <xifrolanor@gmail.com>
2023-07-28 11:47:35 +0300

    Edit comment

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 //в функцію main встановлюється значення file_to_find(шукаємий файл) та root_directory(корнева директорія для пошуку), далі програма визиває функцію search_file з цими параметрами і виводить повідомлення
 int main() {
   const char *file_to_find = "target_file.txt";
-  const char *root_directory = "/"; // Ищем в текущей директории
+  const char *root_directory = "/"; // Шукаємо з корневої директорії
 
   if (search_file(file_to_find, root_directory))
     printf("Пошук завершено.\n");
